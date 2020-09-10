887f8cc20646995ca97781a9bcbe626dee845118
Alan Campos Izaguirre <alan@blacklion.mx>
2020-09-10 12:45:40 -0500

    Re-factores test and ingredients

--- app/recipe/test/test_ingredents_api.py
+++ app/recipe/test/test_ingredents_api.py
@@ -80,3 +80,5 @@ class PrivateIngredientsAPITests(TestCase):
         """Test creating invalid ingredient fails"""
         payload = {'name': ''}
         res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
