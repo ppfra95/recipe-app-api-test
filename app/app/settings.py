45d332abc2bab0f4b24fd3f0475817a617baabc8
Alan Campos Izaguirre <alan@blacklion.mx>
2020-09-15 10:05:01 -0500

    delete docker feature

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'test',
+        'USER': 'chikitin',
+        'PASSWORD': '9809028JXV',
+        'PORT': '5432',
     }
 }
 
